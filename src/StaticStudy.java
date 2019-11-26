c4aef1b6b8658cc2be4f551b69f7547fca41f311
suyuesheng <sys088519@163.com>
2019-11-26 13:57:02 +0800

    对static做了一些修改

--- src/StaticStudy.java
+++ src/StaticStudy.java
@@ -2,12 +2,20 @@ class Boook {//随意定义一个类
     private int value;
     private String name;
     static String pub = "科大出版社";//static,在类里面定义公共的属性，它可以统一修改，并只占一个内存。从而达到方便修改和少占内存的目的
+    public Boook(){
+
+    }
     public Boook(int value, String name) {
         this.name = name;
         this.value = value;
     }
     public String getInfo(){
         return "|-"+"价格："+this.value+"\t书名："+this.name+"\t出版社："+this.pub;
+    }
+    //static方法，可以直接通过类名称来调用
+    //static方法只能调用static调用的属性或方法
+    public static void setPub(String p){
+        pub=p;//不要用this
     }
 }
 
@@ -45,5 +53,17 @@ public class StaticStudy {
           |-价格：19	书名：国语	出版社：济南出版社
           |-价格：14	书名：英文	出版社：济南出版社
         * */
+        Boook.setPub("光明出版社");//static方法，可以直接通过类名称来调用
+        System.out.println(a1.getInfo());
+        System.out.println(a2.getInfo());
+        System.out.println(a3.getInfo());
+        /*
+        * |-价格：12	书名：数学	出版社：光明出版社
+          |-价格：19	书名：国语	出版社：光明出版社
+          |-价格：14	书名：英文	出版社：光明出版社
+        * */
+        System.out.println(new Boook().pub);//新对象依然是光明出版社
+        Boook bb =new Boook(12,"dd");
+        System.out.println(Boook.pub);/*光明出版社*/
     }
 }
