ae863f58f14ac822e0c8cfef7c741226b64ba7da
suyuesheng <sys088519@163.com>
2019-11-30 23:57:06 +0800

    增加覆盖覆写的内容

--- src/JjCheng.java
+++ src/JjCheng.java
@@ -1,6 +1,8 @@
 class Presonn{
     private String name;
     private int age;
+    //属性覆盖
+    int id=1;
     public void setName(String name){
         this.name=name;
     }
@@ -13,11 +15,17 @@ class Presonn{
     public int getAge(){
         return this.age;
     }
+    //方法覆写，如果父类和子类有相同的方法的话就需要用到方法覆写
+    //需要注意，方法覆写的返回值要保持一致，这和方法的重载是一样的
+    public void fun(){
+        System.out.println("father>>>>>fun()");
+    }
     public Presonn(){
         System.out.println("调用了父类的无参构造");
     }
 }
 class Stud extends Presonn{
+    int id=2;
     private String schoolname;
     public void setSchoolname(String schoolname){
         this.schoolname=schoolname;
@@ -36,8 +44,11 @@ class Stud extends Presonn{
        Error:(29, 32) java: age 在 presonn 中是 private 访问控制
      */
     public void fun(){//但字类可以通过其他方法来合理地调用父类的私有属性或方法
+        super.fun();//调用父类的方法
+        System.out.println("child>>>>fun()");
         System.out.println(this.getAge());
         System.out.println(this.getName());
+        System.out.println(super.id);//调用父类的属性
     }
     //实例化子类就默认调用了父类的无参构造
     /*给父类的构造方法写一个输出语句，也给子类写一个就知道了
@@ -62,6 +73,7 @@ public class JjCheng {
         s1.setName("li");
         s1.setSchoolname("春风小学");
         System.out.println(s1.getSchoolname());
-        s1.fun();
+        s1.fun();//调用子类的方法,因为属性的覆盖，这里的id为1
+        System.out.println(s1.id);//这里的id为2
     }
 }
